6a362a59d5264d12cf1090b1b27be86f1e963f90
Omar Abohashim <ohashim530@gmail.com>
2026-07-03 05:12:09 +0300

    create a user entity

---
+++ src/main/java/com/learn/blog/domain/entities/User.java
@@ -0,0 +1,50 @@
+package com.learn.blog.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(createdAt, user.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, name, createdAt);
+    }
+
+    @PrePersist
+    protected void onCreate (){
+        this.createdAt = LocalDate.now();
+    }
+}
